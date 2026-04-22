a320b0396370522eb52e3dbb4f8728144e6a6e08
ashikurriyal <riyal.ashikur@gmail.com>
2026-04-22 23:50:49 +0600

    factorial question solved

---
+++ G_Factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        long long int f = 1;
+        for (int j = 1; j <= n; j++)
+        {
+            f = f * j;
+        }
+        printf("%d\n", f);
+    }
+    return 0;
+}
